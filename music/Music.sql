2d3aae85cbabdb30607dcf134ece2707e2cd2ce1
BBatenin <BBatenin@cdtnue.onmicrosoft.com>
2026-06-24 14:00:08 +0200

    update

--- music/Music.sql
+++ music/Music.sql
@@ -18,3 +18,28 @@ FOREIGN KEY (Genre_id) REFERENCES genre(Genre_id),
 );
 
 
+CREATE TABLE Veranstaltungsort (
+v_id INT IDENTITY(1,1) PRIMARY KEY,
+ort_name VARCHAR(200)
+);
+
+
+CREATE TABLE in_ver   (
+interpret_id INT,
+v_id INT NOT NULL,
+datum_von DATE,
+datum_bis DATE,
+FOREIGN KEY (interpret_id) REFERENCES interpret(interpret_id),
+CONSTRAINT pk_in_ver PRIMARY KEY (interpret_id, datum_von, datum_bis)
+);
+
+drop TABLE in_ver;
+drop TABLE Veranstaltungsort;
+drop TABLE interpret;
+drop TABLE genre;
+drop TABLE TITEL;
+
+
+select * from titel;
+select * from interpret;
+select * from in_ver;
