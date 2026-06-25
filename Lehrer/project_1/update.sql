40c5d591a7816e9d0c536d339ca9cd831e8dd973
BBatenin <BBatenin@cdtnue.onmicrosoft.com>
2026-06-25 09:56:17 +0200

    taabelle aus tabelle

--- Lehrer/project_1/update.sql
+++ Lehrer/project_1/update.sql
@@ -71,3 +71,86 @@ insert into mein_test4 values (3, 'Lukas'), (2, 'null'), (-4, Null), (25, null);
 
 
 select *
+
+
+--Tabelle aus Tabelle selektiv
+
+select * from kermit_archive
+
+create table kermit_archive (
+nummer INT,
+vname varchar(10)
+);
+
+insert into kermit_archive 
+select * from kermit;
+
+CREATE TABLE kermit (
+id INT IDENTITY(1,1) PRIMARY KEY,
+vorname VARCHAR(100)
+);
+
+drop table kermit
+
+INSERT INTO kermit VALUES
+('Kermit'),
+('Piggiee'),
+('Gonzo'),
+('NULL'),
+('null'),
+('Riz-zo'),
+('Kermit'),
+('Waldorf'),
+('Piggiee'),
+('Waldorf'),
+('Gonzo'),
+('NULL'),
+('null'),
+('Riz-zo'),
+('Waldorf'),
+('Waldorf');
+
+select * from kermit_archive
+select * from kermit
+
+delete from kermit_archive
+
+drop table kermit
+
+select * from kermit
+
+insert into kermit_archive
+select * from kermit
+where vorname <> 'Kermit' and vorname != 'null'
+and vorname is not null
+
+
+insert into kermit_archive
+select * from kermit
+where vorname <> 'Kermit' and vorname != 'null'
+and vorname is not null
+
+
+create table kermit_archive_2 (
+id int identity(1,1), 
+vname VARCHAR(10)
+);
+
+insert into kermit_archive_2
+select vorname from kermit;
+
+select * from kermit_archive_2;
+
+select * into kermit_archive_3 from kermit;
+
+select * from kermit_archive_3;
+
+select * into neu_kermit from kermit_archive_3;
+
+select * from neu_kermit;
+
+
+insert into neu_kermit values ('afdsalfjfjf');
+
+
+
