4b04ed98b678d37f37418f677aa3998dc4cf9bf2
BBatenin <BBatenin@cdtnue.onmicrosoft.com>
2026-06-29 12:11:11 +0200

    Add SQL queries to check availability for 'Miles Davis' on 01.07.2027

--- DATUM/datum_uebung.sql
+++ DATUM/datum_uebung.sql
@@ -42,3 +42,143 @@ select top 1 * from in_ver;
 select * from in_ver;
 
 
+select titel.titel, genre.genre
+from titel
+join genre on titel.genre_id = genre.g_id
+where genre.genre = 'Klassik';
+
+SELECT
+  i.name,
+  v.ort_name,
+  iv.datum_von,
+  iv.datum_bis
+FROM in_ver iv
+JOIN interpret i ON iv.i_nr = i.i_id
+JOIN veranstaltungsort v ON iv.v_nr = v.v_id
+WHERE i.name = 'Miles Davis'
+  AND '2027-07-01' BETWEEN iv.datum_von AND iv.datum_bis;
+
+  select i.name, iv.datum_von, iv.datum_bis
+from in_ver iv
+join interpret i on i.i_id = iv.i_nr
+where i.name like 'Miles%'
+and '2027-07-01' between iv.datum_von and iv.datum_bis;
+
+
+
+  SELECT
+  CASE
+    WHEN EXISTS (
+      SELECT 1
+      FROM in_ver iv
+      JOIN interpret i ON iv.i_nr = i.i_id
+      WHERE i.name = 'Miles Davis'
+        AND '2027-07-01' BETWEEN iv.datum_von AND iv.datum_bis
+    )
+    THEN 'Nein, Miles Davis ist bereits gebucht'
+    ELSE 'Ja, Miles Davis kann gebucht werden'
+  END AS buchbar;
+
+
+select i. name, iv.datum_von, iv.datum_bis
+from in_ver iv
+join interpret i on i.i_id = iv.i_nr
+where i. name = 'Miles Davis'
+-- where i.name like, 'Miles%'
+and iv.datum_von <= '2027-07-01'
+and iv.datum_bis >= '2027-07-01';
+-- and '2027-07-01' between iv.datum_von and iv.datum_bis;
+
+
+-- 4. Ich meine mich zu erinnern,
+-- dass Miles Davis am 30.06.2026 bereits irgendwo
+-- gebucht wurde. Ermitteln Sie an welchem
+-- Veranstaltungsort er auftreten wird.
+
+
+SELECT
+  i.name,
+  v.ort_name AS veranstaltungsort,
+  iv.datum_von,
+  iv.datum_bis
+FROM in_ver iv
+JOIN interpret i
+  ON iv.i_nr = i.i_id
+JOIN veranstaltungsort v
+  ON iv.v_nr = v.v_id
+WHERE i.name = 'Miles Davis'
+  AND '2026-06-30' BETWEEN iv.datum_von AND iv.datum_bis;
+
+
+  SELECT
+  i.name,
+  v.ort_name AS veranstaltungsort,
+  iv.datum_von,
+  iv.datum_bis
+FROM in_ver iv
+JOIN interpret i
+  ON iv.i_nr = i.i_id
+JOIN veranstaltungsort v
+  ON iv.v_nr = v.v_id
+WHERE i.name = 'Miles Davis'
+  AND '2027-07-01' BETWEEN iv.datum_von AND iv.datum_bis;
+
+
+  SELECT
+  interpret.name,
+  veranstaltungsort.ort_name
+FROM in_ver
+JOIN interpret
+  ON in_ver.i_nr = interpret.i_id
+JOIN veranstaltungsort
+  ON in_ver.v_nr = veranstaltungsort.v_id
+WHERE interpret.name = 'Miles Davis'
+  AND '2026-06-30' BETWEEN in_ver.datum_von AND in_ver.datum_bis;
+
+
+  SELECT
+  interpret.name,
+  veranstaltungsort.ort_name,
+  in_ver.datum_von,
+  in_ver.datum_bis
+FROM in_ver
+JOIN interpret
+  ON in_ver.i_nr = interpret.i_id
+JOIN veranstaltungsort
+  ON in_ver.v_nr = veranstaltungsort.v_id
+WHERE interpret.name = 'Miles Davis';
+
+-- 4. Ich meine mich zu erinnern,
+-- dass Miles Davis am 30.06.2026 bereits irgendwo
+-- gebucht wurde. Ermitteln Sie an welchem
+-- Veranstaltungsort er auftreten wird. Und am 2027-03-08?
+-- Und alle Veranstaltungen im 2026?
+
+
+SELECT
+  interpret.name,
+  veranstaltungsort.ort_name,
+  in_ver.datum_von,
+  in_ver.datum_bis
+FROM in_ver
+JOIN interpret
+  ON in_ver.i_nr = interpret.i_id
+JOIN veranstaltungsort
+  ON in_ver.v_nr = veranstaltungsort.v_id
+WHERE in_ver.datum_von BETWEEN '2026-01-01' AND '2026-12-31'
+ORDER BY in_ver.datum_von;
+
+
+SELECT
+  interpret.name,
+  veranstaltungsort.ort_name,
+  in_ver.datum_von,
+  in_ver.datum_bis
+FROM in_ver
+JOIN interpret
+  ON in_ver.i_nr = interpret.i_id
+JOIN veranstaltungsort
+  ON in_ver.v_nr = veranstaltungsort.v_id
+WHERE interpret.name = 'Miles Davis'
+  AND in_ver.datum_von BETWEEN '2026-01-01' AND '2026-12-31'
+ORDER BY in_ver.datum_von;
